0f62da312606f0debcee228c9c5f9ce5f9b2224f
ebazazzadeh <ebazazzadeh@gmail.com>
2021-10-01 19:49:43 -0700

    final update for signup page

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,22 +1,93 @@
 const router = require('express').Router();
-const { User, Track } = require('../../models');
+const { User, Tracker } = require('../../models');
 const withAuth = require ('../../utils/auth');
+
+//////////////////// Elham Added //////////////////////
+// creates a new user account when the user signed up for a new account.
 
 router.post('/', async(req, res) => {
     try {
-        const userData = await User.create(req.body);
+        console.log("***********************************************************");
+        //first we check to see if the user is already in the database.
+        let userData = await User.findOne({
+            where: {
+                // full_name: req.body.name,
+                email: req.body.email,
+            }
+        })
+        console.log("////////////");
+        console.log(req.body);
+        //if the user is not existed in the database, we create a new user.
+        if (!userData) {
+            userData = await User.create(req.body);
+            console.log('user email :' + userData.email);
+        }
 
+        const user = userData.get({ plain: true });
+
+            //       const newUser= await User.create({
+            // full_name: req.body.name,
+            // email: req.body.email,
+            // password: req.body.password,
+            // current_weight: req.body.currentWeight,
+            // target_weight :req.body.targetWeight,
+            // start_date: req.body.startDate,
+            // target_date: req.body.targetDate,
+            //       })
+
+        //save the user info to the session storage.
         req.session.save(() => {
-            //req.session.user_id = userData.id;
-            req.session.user_id = userData.id;
+            req.session.user_id = user.id;
+            req.session.user_name = user.name;
+            req.session.user_email = user.email;
             req.session.logged_in = true;
-
-            res.status(200).json(userData);
+            res.status(200).json(user);
         });
     } catch (err) {
         res.status(400).json(err);
     }
 });
+
+
+
+// CREATE a New User
+// router.post('/signup', withAuth, async(req, res) => {
+//     try {
+//           const newUser= await User.create({
+//             full_name: req.body.name,
+//             email: req.body.email,
+//             password: req.body.password,
+//             current_weight: req.body.currentWeight,
+//             target_weight :req.body.targetWeight,
+//             start_date: req.body.startDate,
+//             target_date: req.body.targetDate,
+//         });
+
+//         res.status(200).json(newUser);
+//     } catch (err) {
+//         console.error(err);
+//         res.status(400).json(err);
+//     }
+// });
+
+
+/////////////////////////////End Elham////////////////////////////////////////
+
+// router.post('/', async(req, res) => {
+//     try {
+//         const userData = await User.create(req.body);
+
+//         req.session.save(() => {
+//             //req.session.user_id = userData.id;
+//             req.session.user_id = userData.id;
+//             req.session.logged_in = true;
+
+//             res.status(200).json(userData);
+//         });
+//     } catch (err) {
+//         res.status(400).json(err);
+//     }
+// });
 
 router.post('/login', async(req, res) => {
     try {
