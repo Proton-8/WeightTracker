9f2e77c4ce310aeb0f6f4a99537641fde0dd8e1f
doublerainbow <ly.thuongmy71@gmail.com>
2021-10-02 01:05:48 -0700

    css update

--- controllers/api/trackerRoutes.js
+++ controllers/api/trackerRoutes.js
@@ -6,10 +6,11 @@ const withAuth = require ('../../utils/auth');
 // CREATE a New Track for User
 router.post('/', withAuth, async(req, res) => {
     try {
-          const newTracker= await User.create({
-            user_id: req.params.id,
-            track_date : req.body.track_date,
-            daily_weight:req.body.daily_weight,
+        console.log(req.body);
+          const newTracker= await Tracker.create({
+            user_id: req.session.user_id,
+            track_date: req.body.track_date,
+            daily_weight: req.body.daily_weight,
         });
 
         res.status(200).json(newTracker);
--- public/js/tracker.js
+++ public/js/tracker.js
@@ -5,6 +5,38 @@ const datePickHandler = (event) => {
   $(".datepicker-13").datepicker("show");
 };
 
+
+
+
+const dailyTrackHandler = async (event) => {
+    event.preventDefault();
+  
+    const daily_weight = document.querySelector("#tracker-weight").value.trim();
+    const track_date = document.querySelector("#tracker-date").value.trim();
+  
+    if (daily_weight && track_date) {
+      const response = await fetch("/api/trackers", {
+        method: "POST",
+        body: JSON.stringify({
+         daily_weight,
+         track_date,
+        }),
+        headers: { "Content-Type": "application/json" },
+      });
+      if (response.ok) {
+        document.location.replace("/trackers");
+      } else {
+        alert("response.statusText");
+      }
+    }
+};
+
+document
+  .querySelector("#daily-tracker")
+  .addEventListener("click", dailyTrackHandler);
+
+
+
 document
   .querySelector(".datepicker-13")
   .addEventListener("click", datePickHandler);
